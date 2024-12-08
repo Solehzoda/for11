39129f8dfe80a8c885d7b31d2ccc62cc8495b695
Solehzoda <salikhovajillo@gmail.com>
2024-12-08 11:46:27 +0500

    первый коммит

---
+++ for11/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace for11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число N: ");
+            double N = Convert.ToDouble(Console.ReadLine());
+
+            double sum = 0.0;
+
+            for (double i = N; i <= 2 * N; i++)
+            {
+                sum += i * i;
+            }
+
+            Console.WriteLine($"Сумма будет {sum}");
+        }
+    }
+}
